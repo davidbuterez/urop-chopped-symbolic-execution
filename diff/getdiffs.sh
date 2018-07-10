7bcb6b1c35608638670b05286694ace9a5b6d80b
David Buterez <db2816@ic.ac.uk>
2018-07-10 15:13:50 +0100

    Improved diff script to avoid cloning repository each time

--- diff/getdiffs.sh
+++ diff/getdiffs.sh
@@ -1,19 +1,52 @@
 #!/usr/bin/env bash
 
-REPO=$1
-PATCH_HASH=$2
+repo=$1
+patch_hash=$2
 
-git clone "$REPO" temp > /dev/null 2>&1
-echo "Cloned repository."
+if [[ ! -d temp ]] ; then
+  git clone "$repo" temp > /dev/null 2>&1
+  echo "Cloned repository."
+else
+  need_update=false
+
+  cd temp
+
+  urls=( $(git remote -v) )
+  cloned_url="${urls[1]}"
+
+  if [[ "$repo" != "$cloned_url" ]] ; then
+    echo "Existing temp directory does not match provided repo."
+    cd ..
+    rm -r temp
+    git clone "$repo" temp > /dev/null 2>&1
+    need_update=true
+  fi
+
+  current_commit="$(git show -s --format=%H)"
+  last_commit_info=( $(git log HEAD^..HEAD) )
+
+  last_commit="${last_commit_info[1]}"
+
+  if [[ "$current_commit" != "$last_commit" ]] ; then
+    echo "Repository out of date. Pulling..."
+    git pull
+    need_update=true
+  fi
+
+  if [[ "$need_update" == false ]] ; then
+    echo "Everything up-to-date."
+  fi
+
+  cd ..
+fi
 
 cd temp
 
-git diff "$PATCH_HASH~1" "$PATCH_HASH" -U0 >../diffs
+git diff "$patch_hash~1" "$patch_hash" -U0 >../diffs
 echo "Wrote diff file."
 echo
 
 cd ..
-rm -rf temp
 
 #Analyze diff
 declare -r new_file_regex="\+\+\+ b/(.*)"
@@ -21,41 +54,41 @@ declare -r hunk_regex="@@.*\+([0-9]+),([0-9]+) @@"
 declare -r hunk_def_regex="@@.*\+([0-9]+) @@"
 declare -r target_file_type=".*\.c$|.*\.h$"
 
-DIFF=( )
-readarray DIFF < diffs
+diff_array=( )
+readarray diff_array < diffs
 
-FIRST_FILE=1
+first_file=1
 
-for line in "${DIFF[@]}"; do
-  SLINE=0
+for line in "${diff_array[@]}"; do
+  sline=0
   if [[ $line =~ $new_file_regex ]]; then
     F=${BASH_REMATCH[1]}
     F=`echo -e $F`
     if [[ $F =~ $target_file_type ]]; then
-      if [ $FIRST_FILE -eq 0 ]; then
+      if [ $first_file -eq 0 ]; then
         echo
-      else FIRST_FILE=0
+      else first_file=0
       fi
       echo "Filename: $F"
       echo "New code at lines: "
     fi
 
   elif [[ $line =~ $hunk_regex ]]; then
-    SLINE=${BASH_REMATCH[1]}
-    CLINE=${BASH_REMATCH[2]}
-    # echo "found new hunk $SLINE $CLINE"
+    sline=${BASH_REMATCH[1]}
+    cline=${BASH_REMATCH[2]}
+    # echo "found new hunk $sline $cline"
 
   elif [[ $line =~ $hunk_def_regex ]]; then
-    SLINE=${BASH_REMATCH[1]}
-    CLINE=1
-    # echo "found new dhunk $SLINE $CLINE"
+    sline=${BASH_REMATCH[1]}
+    cline=1
+    # echo "found new dhunk $sline $cline"
 
   fi
 
-  if [[ $SLINE -ne 0 ]]; then
+  if [[ $sline -ne 0 ]]; then
     if [[ $F =~ $target_file_type ]]; then
-      i=$SLINE
-      let "LLINE = SLINE + CLINE"
+      i=$sline
+      let "LLINE = sline + cline"
       while [[ $i -lt $LLINE ]]; do
         echo -n "$i "
         let i++
