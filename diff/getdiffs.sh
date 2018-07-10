7d7cb9e398ccde4f24244ad771793125872cbee4
David Buterez <db2816@ic.ac.uk>
2018-07-10 17:06:42 +0100

    Improved diff script output format - now also includes funtion names where code was patched

--- diff/getdiffs.sh
+++ diff/getdiffs.sh
@@ -43,7 +43,7 @@ fi
 cd temp
 
 git diff "$patch_hash~1" "$patch_hash" -U0 >../diffs
-echo "Wrote diff file."
+echo "Created diffs file."
 echo
 
 cd ..
@@ -61,6 +61,8 @@ first_file=1
 
 for line in "${diff_array[@]}"; do
   sline=0
+  fn_name=""
+  
   if [[ $line =~ $new_file_regex ]]; then
     F=${BASH_REMATCH[1]}
     F=`echo -e $F`
@@ -69,13 +71,20 @@ for line in "${diff_array[@]}"; do
         echo
       else first_file=0
       fi
-      echo "Filename: $F"
-      echo "New code at lines: "
+      echo "Patched lines: "
     fi
 
   elif [[ $line =~ $hunk_regex ]]; then
     sline=${BASH_REMATCH[1]}
     cline=${BASH_REMATCH[2]}
+
+    # fn_name="${line%(*}"
+    # fn_name="${fn_name##* }"
+
+    fn_name=${line#*@@}
+    fn_name=${line#* @@}
+    fn_name=${fn_name%{*}
+    fn_name=$(echo $fn_name | xargs)
     # echo "found new hunk $sline $cline"
 
   elif [[ $line =~ $hunk_def_regex ]]; then
@@ -85,20 +94,33 @@ for line in "${diff_array[@]}"; do
 
   fi
 
+  COLOR1='\033[0;32m'
+  COLOR2='\033[1;34m'
+  NC='\033[0m'
+
   if [[ $sline -ne 0 ]]; then
     if [[ $F =~ $target_file_type ]]; then
       i=$sline
       let "LLINE = sline + cline"
+
+      if [[ -z "$fn_name" ]] ; then
+        printf "${COLOR2}$F${NC}: Outside any function, at lines ["
+      else
+        printf "${COLOR2}$F${NC}: In function ${COLOR1}$fn_name${NC} at lines ["
+      fi
+
       while [[ $i -lt $LLINE ]]; do
-        echo -n "$i "
+        if [[ $i == $(($LLINE - 1)) ]] ; then
+          echo -n "$i"
+        else
+          echo -n "$i, "
+        fi
         let i++
       done
+
+      echo "]"
     fi
   fi
 done
 
 echo
-echo
-echo "Analyzed diff"
-
-
