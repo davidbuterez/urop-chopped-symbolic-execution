283431a9c13fb93038ab37808e09c8d696424ced
David Buterez <db2816@ic.ac.uk>
2018-07-14 15:10:22 +0100

    Improved printing functionality and refactored print code

--- diff/python/getdiffs.py
+++ diff/python/getdiffs.py
@@ -12,6 +12,26 @@ import json
 from os.path import dirname, abspath
 from termcolor import colored
 
+class DiffLines:
+
+  def __init__(self, added_lines, removed_lines):
+    self.added_lines = added_lines
+    self.removed_lines = [-ln for ln in removed_lines]
+
+  def print_added_lines(self):
+    print()
+    if self.added_lines:
+      print('Patch has added lines (new file indices): [', end='')
+      print(*self.added_lines, end='')
+      print(']', end='')
+
+  def print_removed_lines(self):
+    print()
+    if self.removed_lines:
+      print('Patch has removed lines (old file indices): [', end='')
+      print(*self.removed_lines, end='')
+      print(']', end='')
+
 class FnData:
 
   def __init__(self, fn_name, start, end, prototype):
@@ -27,12 +47,11 @@ class FnData:
 
 class FileDiff:
   
-  def __init__(self, filename, restrict):
+  def __init__(self, filename):
     self.filename = filename
-    self.restrict_extensions = restrict
     self.file_extension = filename[filename.rfind('.'):]
     self.fn_map = self.get_fn_names()
-    self.fn_to_lines = {key: [] for key in self.fn_map.keys()}
+    self.fn_to_lines = {}
 
   def get_fn_names(self):
     fn_table = subprocess.check_output(['ctags', '-x', '--c-kinds=fp', '--fields=+ne', '--output-format=json', self.filename]).decode('utf-8').strip().split('\n')
@@ -45,49 +64,33 @@ class FileDiff:
     return fn_map
 
   def match_lines_to_fn(self, lines):
-    # matched_fn_name = None
-
-    start, end = 0, 0
-
     for fn_name, fn_data in self.fn_map.items():
+      added, removed = [], []
       for line_no in lines:
-        if line_no >= fn_data.start_line and line_no <= fn_data.end_line:
-          self.fn_to_lines[fn_name].append(line_no)
+        if abs(line_no) >= fn_data.start_line and abs(line_no) <= fn_data.end_line:
+          if line_no > 0:
+            added.append(line_no)
+          else:
+            removed.append(line_no)
 
-  # Full function information
-  def print_fn_pretty(self, fn_name):
-    if fn_name:
-      print(colored(self.filename, 'blue') + ": " + colored(fn_name, 'green'), end='')
-
-  # Just function name
-  def print_fn_simple(self, fn_name):    
-    if fn_name and fn_name != 'Global':
-      first_extract = fn_name[fn_name.find(' ') + 1:]
-      second_extract = first_extract[:first_extract.rfind('(')]
-      print(colored(second_extract, 'green'))
-
-  @staticmethod
-  def print_lines(lines):
-    print(' changed lines [', end='')
-    print(*lines, end='')
-    print(']')
+      if fn_name in self.fn_to_lines:
+        self.fn_to_lines[fn_name].added_lines.extend(added)
+        self.fn_to_lines[fn_name].removed_lines.extend(removed)
+      elif added or removed:
+        self.fn_to_lines[fn_name] = DiffLines(added, removed)
 
   # Prints all the data that this object has
   def print(self, pretty):
-
-    def select_print(pretty, fn_name, lines):
-      if pretty and lines:
-        self.print_fn_pretty(fn_name)
-        FileDiff.print_lines(lines)
-      elif lines:
-        self.print_fn_simple(fn_name)
+    if not pretty:
+      print('Updated functions:')
 
     for fn_name, lines in self.fn_to_lines.items():
-      if self.restrict_extensions:
-        if self.file_extension in args_orig.exts:
-          select_print(pretty, fn_name, lines)   
-      else:
-        select_print(pretty, fn_name, lines)
+      if pretty and lines:
+        print('%s: In function %s' % (colored(self.filename, 'blue'), colored(fn_name, 'green')), end='')
+        self.fn_to_lines[fn_name].print_added_lines()
+        self.fn_to_lines[fn_name].print_removed_lines()
+      elif lines:
+        print('  %s' % colored(fn_name, 'green'))
 
 # Handle the cloning of a repo
 def clone_repo(repo_url, repo_path):
@@ -118,6 +121,8 @@ def print_diff_summary(diff_summary, pretty):
   for diff_data in diff_summary:
     diff_data.print(pretty)
 
+
+
 ##### Main program #####
 
 # Initialize argparse
@@ -125,7 +130,6 @@ parser = argparse.ArgumentParser(description='Outputs a list of patched function
 
 parser.add_argument('gitrepo', metavar='repo', help='git repo url')
 parser.add_argument('patchhash', help='patch hash')
-parser.add_argument('--file-extensions', dest="exts", default=['.c', '.h'], metavar='ext', nargs='+', help='data about these file extensions (default .c, .h)' )
 parser.add_argument('--only-function-names', dest='fn_names', action='store_true', help='display only a list of function names')
 
 # Dictionary of arguments
@@ -142,12 +146,17 @@ ceil_dir = dirname(abspath(repo_path))
 # Check if we have a repo
 discover_repo_path = pygit2.discover_repository(repo_path, 0, dirname(cwd))
 
+# Allowed file extensions
+extensions = ['.c', '.h']
+
 repo = None
 
 if discover_repo_path is None:
   print("No repo found. Cloning...")
   repo = clone_repo(args['gitrepo'], repo_path)
   print("Cloned repo.")
+  current_hash = repo.revparse_single('HEAD')
+  print('Current commit (patch): ' + current_hash.hex)
 
 else:
   repo = pygit2.Repository(discover_repo_path)
@@ -162,12 +171,14 @@ else:
     repo = clone_repo(args['gitrepo'], repo_path)
 
     print("Cloned repo.")
+    current_hash = repo.revparse_single('HEAD')
+    print('Current commit (patch): ' + current_hash.hex)
   else:
     print('Found required repo.')
     
     # Check that commits match
     current_hash = repo.revparse_single('HEAD')
-    print('Current commit: ' + current_hash.hex)
+    print('Current commit (patch): ' + current_hash.hex)
     if current_hash.hex != args['patchhash']:
       print('Changing to desired commit...')
       hash_oid = pygit2.Oid(hex=args['patchhash'])
@@ -179,33 +190,47 @@ os.chdir(repo_path)
 
 # Get diff between patch commit and previous commit
 prev = repo.revparse_single('HEAD~')
-diff = repo.diff(prev, context_lines=0)
+curr = repo.revparse_single('HEAD')
+print("Comparing with previous commit: " + prev.hex)
+diff = repo.diff(prev, curr, context_lines=0)
 
 # Write diff file
 diff_file = open('diffs', 'w')
 diff_file.write(diff.patch)
 diff_file.close()
 
-print('Displaying patch information:\n')
-
 diff_summary = []
 patches = list(diff.__iter__())
 
 for patch in patches:
   filename = patch.delta.new_file.path
-  diff_data = FileDiff(filename, args_orig.exts is not None)
+
+  extension = filename[filename.rfind('.'):]
+  if extension not in extensions:
+    continue
+
+  diff_data = FileDiff(filename)
 
   for hunk in patch.hunks:
     fn_lines = []
     for diff_line in hunk.lines:
-      if diff_line.new_lineno > -1 and diff_line.content.strip():
-        fn_lines.append(diff_line.new_lineno)
+      if diff_line.content.strip():
+        if diff_line.new_lineno > -1:
+          fn_lines.append(diff_line.new_lineno)
+        else:
+          fn_lines.append(-diff_line.old_lineno)
 
     diff_data.match_lines_to_fn(fn_lines)
   
   diff_summary.append(diff_data) 
 
-if bool(args['fn_names']):
-  print_diff_summary(diff_summary, pretty=False)
+if diff_summary:
+  print('Displaying patch information:\n')
+
+  if bool(args['fn_names']):
+    print_diff_summary(diff_summary, pretty=False)
+  else:
+    print_diff_summary(diff_summary, pretty=True)
+  print()
 else:
-  print_diff_summary(diff_summary, pretty=True)
+  print('No relevant changes detected.')
