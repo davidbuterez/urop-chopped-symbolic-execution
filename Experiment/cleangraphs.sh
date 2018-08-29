0c632768c10483247d10741eab3455f418b74515
David Buterez <db2816@ic.ac.uk>
2018-08-29 14:37:56 +0300

    Deleting old files

--- Experiment/cleangraphs.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-cd assembly; rm *; cd ..
-cd call-graph; rm * >/dev/null 2>&1; cd img; rm *; cd ../..
-cd cfg; rm * >/dev/null 2>&1; cd img; rm *; cd ../..
-cd out; rm *; cd ..
